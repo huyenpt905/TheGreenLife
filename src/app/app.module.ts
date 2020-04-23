a6b8f10060e0a53c2fa8d58e7a453ee660c90cd2
Huyen Phan <huyenpt905@gmail.com>
2020-04-23 13:09:39 +0700

    create project structure v1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FooterComponent } from './shared/component/footer/footer.component';
+import { HeaderComponent } from './shared/component/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
